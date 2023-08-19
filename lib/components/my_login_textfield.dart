6f25a787dc951fcbbdb190163fbb305d5b35998e
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2023-08-19 12:06:46 +0800

    Add files via upload

---
+++ lib/components/my_login_textfield.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyLoginTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyLoginTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      // field id
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: SizedBox(
+        width: 400.0,
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)),
+            fillColor: Colors.grey.shade300,
+            filled: true,
+            hintText: hintText,
+          ),
+        ),
+      ),
+    );
+  }
+}
